48e931b4a8b3e8317441f413e0f6ceca460f0c7f
ukasyahf462 <32339489+ukasyahf462@users.noreply.github.com>
2018-04-11 00:42:43 +0700

    Add files via upload

---
+++ googleanalitycs.js
@@ -0,0 +1,7 @@
+ (function(i,s,o,g,r,a,m){i[&#39;GoogleAnalyticsObject&#39;]=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,&#39;script&#39;,&#39;https://www.google-analytics.com/analytics.js&#39;,&#39;ga&#39;);
+
+  ga(&#39;create&#39;, &#39;UA-73803191-4&#39;, &#39;auto&#39;);
+  ga(&#39;send&#39;, &#39;pageview&#39;);
